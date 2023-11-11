e557c2c8d1e96b5a41dc7809fd3ad5253f21fc25
bafto <hendrikziegler@outlook.de>
2023-11-11 12:11:52 +0100

    fixed docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+
 tar czf ./DDP.tar.gz -C $DDPPATH .
 
-if [ $# -eq 0 ] then
+if [ $# -eq 0 ] ; then
 	docker build --build-arg ddppath=./DDP.tar.gz  --tag ddp-spielplatz .
-else if [ $# -eq 2 ] then
+elif [ $# -eq 2 ] ; then
 	cert_path=./$(basename $1)
 	key_path=./$(basename $2)
 	cp $1 $cert_path
 	cp $2 $key_path
-	docker build --build-arg ddppath=./DDP.tar.gz --build-arg certpath=$cert_path --build-arg keypath=$key_path --tag ddp-spielplatz
-else then
+	docker build --build-arg ddppath=./DDP.tar.gz --build-arg certpath=$cert_path --build-arg keypath=$key_path --tag ddp-spielplatz .
+else
 	echo "Usage: docker-build.sh [cert_path key_path]"
 fi
