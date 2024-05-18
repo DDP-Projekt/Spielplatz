6ed2150e5cc54f711039d4d22e739de35311cbcb
bafto <hendrikziegler@outlook.de>
2024-05-18 16:36:23 +0200

    added structured logging to server.go

--- server/server.go
+++ server/server.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
+	"context"
 	"fmt"
-	"log"
+	"log/slog"
 	"net/http"
 	"strconv"
 	"time"
@@ -14,11 +14,25 @@ import (
 	"github.com/DDP-Projekt/Spielplatz/server/kddp"
 	wsrw "github.com/DDP-Projekt/Spielplatz/server/websocket_rw"
 	gin_pprof "github.com/gin-contrib/pprof"
+	"github.com/gin-contrib/requestid"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	"github.com/spf13/viper"
 	lslogging "github.com/tliron/commonlog"
 )
+
+func fatal(msg string, args ...any) {
+	slog.Log(context.Background(), slog.LevelError+4, msg, args...)
+	panic(fmt.Errorf(msg))
+}
+
+func getLogger(c *gin.Context) *slog.Logger {
+	if logger, ok := c.Get("logger"); ok {
+		return logger.(*slog.Logger)
+	}
+	slog.Error("no logger in gin context")
+	return slog.Default()
+}
 
 func setup_config() {
 	viper.SetDefault("exe_cache_duration", time.Second*60)
@@ -37,24 +51,36 @@ func setup_config() {
 	viper.SetConfigType("json")
 	viper.AddConfigPath(".")
 	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalf("Error reading config file: %s\n", err)
+		fatal("Error reading config file", "err", err)
 	}
 
-	json_string, err := json.MarshalIndent(viper.AllSettings(), "", "\t")
-	if err == nil {
-		log.Printf("using config: \n%s\n", json_string)
+	settings := viper.AllSettings()
+	settings_log := make([]any, 0, len(settings))
+	for k, v := range settings {
+		settings_log = append(settings_log, k, v)
 	}
+	slog.Info("configuration done", settings_log...)
 }
 
 func main() {
 	setup_config()
 
 	if err := kddp.InitializeSemaphore(viper.GetInt64("max_concurrent_processes")); err != nil {
-		log.Fatalf("could not initialize semaphore: %s\n", err)
+		fatal("failed to initialize semaphore", "err", err)
 	}
 
 	r := gin.New()
-	r.Use(gin.Recovery())
+	r.Use(
+		gin.Recovery(),
+		requestid.New(),
+		func(c *gin.Context) {
+			c.Set("logger",
+				slog.Default().
+					With("X-Request-ID", requestid.Get(c)).
+					With("ip", c.ClientIP()),
+			)
+		},
+	)
 
 	// load html files as template
 	r.LoadHTMLGlob("static/html/*")
@@ -88,14 +114,25 @@ func main() {
 	}
 
 	// run the server
-	log.Println("starting server")
-	if viper.GetBool("useHTTPS") {
-		if viper.GetString("certPath") == "" || viper.GetString("keyPath") == "" {
-			log.Fatal("certPath and keyPath can not be empty!")
+
+	var (
+		use_https = viper.GetBool("useHTTPS")
+		cert_path = viper.GetString("certPath")
+		key_path  = viper.GetString("keyPath")
+		port      = viper.GetString("port")
+	)
+	slog.Info("starting server", "https", use_https)
+	if use_https {
+		if cert_path == "" || key_path == "" {
+			fatal("certPath and keyPath can not be empty!", "certPath", cert_path, "keyPath", key_path)
 		}
-		log.Fatal(r.RunTLS(":"+viper.GetString("port"), viper.GetString("certPath"), viper.GetString("keyPath")))
+		if err := r.RunTLS(":"+port, cert_path, key_path); err != nil {
+			fatal("failed to run server", "err", err)
+		}
 	} else {
-		log.Fatal(r.Run(":" + viper.GetString("port")))
+		if err := r.Run(":" + port); err != nil {
+			fatal("failed to run server", "err", err)
+		}
 	}
 }
 
@@ -103,32 +140,36 @@ var upgrader = websocket.Upgrader{}
 
 // serves the /ls endpoint
 func serve_ls(c *gin.Context) {
-	log.Printf("new connection to %s\n", c.ClientIP())
+	logger := getLogger(c)
+	logger.Info("new connection")
 	// upgrade the connection to a websocket connection
 	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
-		log.Println(err)
+		logger.Error("failed to initialize websocket connection")
 		return
 	}
 	defer ws.Close()
 
 	ls := ddpls.NewDDPLS()
 	ls.Server.ServeWebSocket(ws)
-	log.Printf("connection with %s closed\n", c.ClientIP())
+	logger.Info("language server connection closed")
 }
 
 // serves the /compile endpoint
 func serve_compile(c *gin.Context) {
+	logger := getLogger(c)
 	type CompileRequest struct {
 		Src string `json:"src"`
 	}
 
-	log.Printf("new compilation request from %s\n", c.ClientIP())
+	logger.Info("got compilation request")
 	token, exe_path := executables.GenerateExeToken()
+	logger = logger.With("token", token)
+	logger.Info("generated token")
 	// read the src json property from the request body
 	var req CompileRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
-		log.Println(err)
+		logger.Error("unmarshaling request", "err", err)
 		executables.Delete(token)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -138,20 +179,23 @@ func serve_compile(c *gin.Context) {
 	// compile the program
 	result, exe_path, err := kddp.CompileDDPProgram(src_code, token, exe_path)
 	if err != nil {
-		log.Println(err)
+		logger.Error("compiling program", "err", err)
 		executables.Delete(token)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 	executables.Set(token, exe_path)
 
-	log.Printf("compilation of program %d finished\n", token)
+	logger.Info("compilation finished")
 	// delete the executable after 3 minutes
 	go func() {
 		dur := viper.GetDuration("exe_cache_duration")
 		time.Sleep(dur)
 		if _, ok := executables.Get(token); ok {
-			log.Printf("executable %s was unused for %s, deleting it", exe_path, dur)
+			logger.Info("executable was unused for cache  duration, deleting it",
+				"exe_path", exe_path,
+				"cache_curation", dur,
+			)
 			executables.RemoveExecutableFile(token, exe_path)
 		}
 	}()
@@ -161,33 +205,38 @@ func serve_compile(c *gin.Context) {
 
 // serves the /run endpoint
 func serve_run(c *gin.Context) {
-	log.Printf("new run request from %s\n", c.ClientIP())
+	logger := getLogger(c)
+	logger.Info("new run request")
 	// upgrade the connection to a websocket connection
 	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
-		log.Println(err)
+		logger.Error("failed to initialize websocket connection")
 		return
 	}
 	defer ws.Close()
 	// get the token from the query
 	token_str, ok := c.GetQuery("token")
 	if !ok {
+		logger.Warn("missing token in run request")
 		// send a close message to the client with error
 		ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseInvalidFramePayloadData, "invalid token"))
 		return
 	}
+	logger.Info("got run request token", "token", token_str)
 	// parse token_str to uint64
 	ti, err := strconv.ParseInt(token_str, 10, 64)
 	token := executables.TokenType(ti)
 	if err != nil {
+		logger.Warn("invalid token")
 		// send a close message to the client with error
 		ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseInvalidFramePayloadData, "invalid token"))
 		return
 	}
+	logger = logger.With("token", token)
 	// get the executable path from the executables map
 	exe_path, ok := executables.Get(token)
 	if !ok {
-		log.Printf("client requested run for invalid token %d", token)
+		logger.Warn("token was invalid")
 		// send a close message to the client with error
 		ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseInvalidFramePayloadData, "invalid token"))
 		return
@@ -199,7 +248,7 @@ func serve_run(c *gin.Context) {
 
 	exitStatus, err := kddp.RunExecutable(exe_path, websocket_rw, websocket_rw.StdoutWriter(), websocket_rw.StderrWriter(), args...)
 	if err != nil {
-		log.Println(err)
+		logger.Error("failed to run executable", "err", err)
 		websocket_rw.Close()
 		// report error to client
 		ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseInternalServerErr, err.Error()))
